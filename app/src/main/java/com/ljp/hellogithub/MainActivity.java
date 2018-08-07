a0d201fa62a827724e5e9e7c1c1125f97260f4a0
ljprxf1314 <13683310026@163.com>
2018-08-07 17:57:07 +0800

    android消息机制

--- app/src/main/java/com/ljp/hellogithub/MainActivity.java
+++ app/src/main/java/com/ljp/hellogithub/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.ljp.hellogithub.activity.animation.AnimationMainActivity;
+import com.ljp.hellogithub.activity.message_mechanism.MessageMechanismActivity;
 import com.ljp.hellogithub.activity.service.ServiceDemoActivity;
 import com.ljp.hellogithub.activity.textview.TextViewSpannableActivity;
 import com.ljp.hellogithub.activity.viewclicksystem.MyViewActivity;
@@ -30,7 +31,7 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.btn_activity, R.id.btn_my_view, R.id.btn_service,R.id.btn_animation,R.id.btn_ui})
+    @OnClick({R.id.btn_activity, R.id.btn_my_view, R.id.btn_service,R.id.btn_animation,R.id.btn_ui,R.id.btn_message})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_activity:
@@ -47,6 +48,9 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.btn_ui:
                 startActivity(UIActivity.class);
+                break;
+            case R.id.btn_message://android消息机制
+                startActivity(MessageMechanismActivity.class);
                 break;
         }
     }
---
+++ app/src/main/java/com/ljp/hellogithub/activity/message_mechanism/MessageMechanismActivity.java
@@ -0,0 +1,65 @@
+package com.ljp.hellogithub.activity.message_mechanism;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.ljp.hellogithub.R;
+import com.ljp.hellogithub.base.BaseActivity;
+
+import java.util.List;
+
+/**
+ * <pre>
+ *     author : lijipei
+ *     time   : 2018/8/7
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+
+public class MessageMechanismActivity extends BaseActivity {
+    //android消息机制:讲解很透彻的文章
+    //http://www.cnblogs.com/codingmyworld/archive/2011/09/12/2174255.html
+
+    private ThreadLocal<Boolean> mBooleanThreadLocal = new ThreadLocal<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_mechanism);
+
+        mBooleanThreadLocal.set(true);
+
+        Log.e("thread","{thread}mBooleanThreadLocal="+mBooleanThreadLocal.get());
+
+        new Thread("thread1"){
+            @Override
+            public void run() {
+                super.run();
+                mBooleanThreadLocal.set(false);
+                Log.e("thread1","{thread1}mBooleanThreadLocal="+mBooleanThreadLocal.get());
+                @SuppressLint("HandlerLeak")
+                Handler hadler = new Handler(){
+                    @Override
+                    public void handleMessage(Message msg) {
+                        super.handleMessage(msg);
+                    }
+                };
+
+                hadler.sendEmptyMessage(1);
+            }
+        }.start();
+
+        new Thread("thread2"){
+            @Override
+            public void run() {
+                super.run();
+                Log.e("thread2","{thread2}mBooleanThreadLocal="+mBooleanThreadLocal.get());
+            }
+        }.start();
+    }
+}
