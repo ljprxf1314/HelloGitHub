a2c9ba4004caefe0309668037a8caab93f3f6a97
ljprxf1314 <13683310026@163.com>
2020-01-03 14:51:52 +0800

    CoordinatorLayout

---
+++ app/src/main/java/com/ljp/hellogithub/ui/coordinator/CoordinatorLayoutActivity.java
@@ -0,0 +1,25 @@
+package com.ljp.hellogithub.ui.coordinator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ljp.hellogithub.R;
+import com.ljp.hellogithub.base.BaseActivity;
+
+/**
+ * <pre>
+ *     author : lijipei
+ *     time   : 2020/1/3
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+
+public class CoordinatorLayoutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_layout);
+    }
+}
